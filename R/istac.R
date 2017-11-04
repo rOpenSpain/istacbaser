50738465f11dcccf42a240fc9260708b6b7be711
jmcartiles <jose.cazorla101@alu.ulpgc.es>
2017-11-04 12:33:14 +0000

    Roxygen nota
    Se incluye nota roxygen

--- R/istac.R
+++ R/istac.R
@@ -1,3 +1,25 @@
+#' Download data from the ISTAC API
+#'
+#' This function downloads the requested information using the ISTAC API.
+#'
+#' @param istac_table Character string with the ID code of the table requested.
+#' This ID code corresponds to the \code{namecode} column from \code{\link{cache}}.
+#' @param islas Character vector of islands codes requested. Default value is special code of \code{all}.
+#' @param label if \code{FALSE}, the data frame returned has the codes used in ISTAC API,
+#' if \code{TRUE}, the data frame returned has the labels used in ISTAC API. Default value is \code{FALSE}.
+#' @param startdate Numeric or character. If numeric it must be in \%Y form (i.e. four digit year).
+#' For data at the subannual granularity the API supports a format as follows: for monthly data, "2016M01"
+#' and for quarterly data, "2016Q1". This also accepts a special value of "YTD", useful for more frequently
+#' updated subannual indicators.
+#' @param enddate Numeric or character. If numeric it must be in \%Y form (i.e. four digit year).
+#' For data at the subannual granularity the API supports a format as follows: for monthly data, "2016M01"
+#' and for quarterly data, "2016Q1".
+#' @param mrv Numeric. The number of Most Recent Values to return. A replacement of \code{startdate} and \code{enddate},
+#' this number represents the number of observations you which to return starting from the most recent date of collection.
+#' Useful in conjuction with \code{freq}.
+#' @return Data frame with all available requested data.
+#'
+#' @export
 istac <- function(istac_table, islas = "all", label = FALSE, startdate, enddate, freq, mrv, POSIXct = FALSE, cache){
 
 
--- R/istac_search.R
+++ R/istac_search.R
@@ -1,6 +1,23 @@
-
-# prueba git
-
+#' Search information about data available through ISTAC API
+#'
+#' This funcion allows finds those ID that match the search term and
+#' returns a data frame with results
+#'
+#' @param pattern Character string or regular expression to be matched.
+#' @param fields Character vector of column names through which to search
+#' @param extra if \code{FALSE}, only the ID and Title are returned,
+#' if \code{TRUE}, all columns of the \code{cache} are returned.
+#' @param cache Data frame with metadata about API and ISTAC information.
+#' @return Data frame with metadata that match the search term.
+#'
+#' @examples
+#' istac_search(pattern = "Superficie")
+#' istac_search(pattern = "Superficie", extra = TRUE)
+#'
+#' istac_search(pattern = "medio ambiente", fields = "Estadísticas por temas")
+#' istac_search(pattern = "medio ambiente", fields = "Estadísticas por temas", extra = TRUE)
+#'
+#' @export
 istac_search <- function(pattern, fields = "title", extra = TRUE, cache){
 
   if (missing(cache)) cache <- istacr::cache
