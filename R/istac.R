e0b01ffb06164d82b2fb2b594dbbfd73b371c7b9
Christiian <aramaki2501@gmail.com>
2017-11-05 23:35:27 +0000

    Añadidos filtros de fechas y mrv
    Falta añadir filtros de islas

--- R/istac.R
+++ R/istac.R
@@ -44,33 +44,53 @@ istac <- function(istac_table, islas = "all", label = FALSE, startdate, enddate,
 
 
 
-  if (!POSIXct)
-    out_df
-  else {
+  if (POSIXct){
 
     date_index <- names(out_df) %in% c("A\u00F1os","Per\u00EDodo")
     vble_date <- names(out_df)[date_index]
 
-    if(any(date_index))
+    if (any(date_index)){
       out_df <- istacperiodos2POSIXct(out_df, vble_date)
-    else
-      warning("The data is no time dependence.")
+      # Creo qu ela siguiente línea no es necesario en nuestro caso.
+      #if (missing(startdate) != missing(enddate)) stop("Using either startdate or enddate requires supplying both. Please provide both if a date range is wanted")
 
-if(label) code2labs
+      if (missing(startdate)) startdate <- min(out_df$fecha) else startdate <- as.Date(startdate,"%d/%m/%Y")
+      if (missing(enddate)) enddate <- max(out_df$fecha) else enddate <- as.Date(enddate,"%d/%m/%Y")
 
-  }
+        # Habrá que comprobar fechas. Formato %d/%m/%Y o %d-%m%-%Y. Comprobar si cumple estos formatos
+        # Habrá que comprobar si sale de rango de fechas. Por ahora vamos a suponer que todo sale bien. 171104
+        index_date <- out_df$fecha >= startdate & out_df$fecha <= enddate
+        out_df <- out_df[index_date, ]
+
+        # check mrv ----------
+        if (!missing(mrv)) {
+
+          if (!is.numeric(mrv)) stop("If supplied, mrv must be numeric")
+
+          if(!missing(stardate) == !missing(mrv)) stop("You can supply only a startdate and enddate or mrv but not both")
+
+          if (missing(freq)){
+            periodon <- min(match(c("anual","cuatrimestral","mensual"),out_df$periodicidad),na.rm = TRUE)
+            periodo <- c("anual","cuatrimestral","mensual")[periodon]
+            enddate <- max(out_df$fecha)
+            startdate <-  min(out_df$fecha)
+            startdate_mrv <- switch(periodo,
+                               "anual" = enddate - lubridate::years(mrv),
+                               "cuatrimestral" = enddate - lubridate::months(4*mrv),
+                               "mensual" = endadte - lubridate::months(mrv))
+            stardate <- ifelse(startdate < startdate_mrv, startdate_mrv, startdate)
+            index_date <- out_df$fecha >= startdate & out_df$fecha <= enddate
+            out_df <- out_df[index_date, ]
+
+          }
+
+        }
+
+      } else
+        warning("The data is no time dependence.")
 
 
-
-  # check dates ----------
-
-  #if (POSIXct & (!missing(startdate) | ! missing(enddate))) stop("stardate and endate are possible only with POSIXct = TRUE")
-  #if (missing(startdate) != missing(enddate))
-  #  stop("Using either startdate or enddate requries supplying both. Please provide both if a date range is wanted")
-  #else
-  # Falta comprobar si stardate y endate están en formato correcto
-  #   out_df <- out_df[out_df$date_ct >= as.Date(stardate, "%d-%m-%Y") & out_df$date_ct <= as.Date(enddate, "%d-%m-%Y"), ]
-
+    }
 
 
   out_df
